fe82718d26f3f6bf2bc246c1d7d81c2a71c4712c
Iuga Stefania <iugastefania26@gmail.com>
2022-05-06 14:54:50 +0300

    Lab 4 source code - spring boot basics

---
+++ aclabs2022/src/main/java/com/aclabs/aclabs2022/Aclabs2022Application.java
@@ -0,0 +1,13 @@
+package com.aclabs.aclabs2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aclabs2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aclabs2022Application.class, args);
+	}
+
+}
---
+++ aclabs2022/src/test/java/com/aclabs/aclabs2022/Aclabs2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aclabs.aclabs2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aclabs2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
